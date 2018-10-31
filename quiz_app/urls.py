b7174656fc8cd9f2bdc106710f6e36be12ac4030
Ignas Grigaravicius <kickeris@gmail.com>
2018-10-31 10:58:17 +0000

    quiz app build on Tom Wolker's app setting up

---
+++ quiz_app/urls.py
@@ -0,0 +1,67 @@
+"""quiz_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import patterns, url
+
+from quiz.views import QuizListView, CategoriesListView,\
+    ViewQuizListByCategory, QuizUserProgressView, QuizMarkingList,\
+    QuizMarkingDetail, QuizDetailView, QuizTake
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+# urlpatterns = [ 
+#     url(r'^admin/', include(admin.site.urls)), 
+#     url(r'', include('blog.urls')), 
+# ]
+urlpatterns = patterns('',
+                       url(r'^admin/', admin.site.urls), 
+                       url(regex=r'^$',
+                           view=QuizListView.as_view(),
+                           name='quiz_index'),
+
+                       url(regex=r'^category/$',
+                           view=CategoriesListView.as_view(),
+                           name='quiz_category_list_all'),
+
+                       url(regex=r'^category/(?P<category_name>[\w|\W-]+)/$',
+                           view=ViewQuizListByCategory.as_view(),
+                           name='quiz_category_list_matching'),
+
+                       url(regex=r'^progress/$',
+                           view=QuizUserProgressView.as_view(),
+                           name='quiz_progress'),
+
+                       url(regex=r'^marking/$',
+                           view=QuizMarkingList.as_view(),
+                           name='quiz_marking'),
+
+                       url(regex=r'^marking/(?P<pk>[\d.]+)/$',
+                           view=QuizMarkingDetail.as_view(),
+                           name='quiz_marking_detail'),
+
+                       #  passes variable 'quiz_name' to quiz_take view
+                       url(regex=r'^(?P<slug>[\w-]+)/$',
+                           view=QuizDetailView.as_view(),
+                           name='quiz_start_page'),
+
+                       url(regex=r'^(?P<quiz_name>[\w-]+)/take/$',
+                           view=QuizTake.as_view(),
+                           name='quiz_question'),
+)
